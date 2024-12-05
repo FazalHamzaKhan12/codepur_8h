14ed4228194cecf7d8312d4096589f98f12f62b5
Fazal Hamza Khan <theunknown.pak@gmail.com>
2024-12-05 14:08:43 +0500

    Chapter 4 Completed
    1:44:02 / 8:38:00 Chaper 4 Completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,10 +6,60 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+        backgroundColor: Colors.white,
         body: Column(
-      children: [Image.asset("assets/images/login_img.png",fit: BoxFit.cover,),
-      Text("Welcome Back",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,),)],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/login_img.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 13.0, horizontal: 25.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter your username", labelText: "Username"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
+                      labelText: "password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login"),
+              style: TextButton.styleFrom(
+                shadowColor: Colors.green,
+                backgroundColor: Colors.white,
+              ),
+            )
+          ],
+        ));
   }
 }
