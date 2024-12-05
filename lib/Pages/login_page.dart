8ece9d61ff3ae149bbf528dcabe903e10dd1ed06
Fazal Hamza Khan <theunknown.pak@gmail.com>
2024-12-05 14:36:30 +0500

    Chapter 5 Completed
    2:04:54 time of youtube video

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:codepur_8h/Utiitz/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -21,45 +22,67 @@ class LoginPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_img.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 13.0, horizontal: 25.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter your username", labelText: "Username"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter your password",
-                      labelText: "password",
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 13.0, horizontal: 25.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter your username",
+                          labelText: "Username"),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter your password",
+                        labelText: "password",
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Login"),
-              style: TextButton.styleFrom(
-                shadowColor: Colors.green,
-                backgroundColor: Colors.white,
+              SizedBox(
+                height: 30,
               ),
-            )
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: TextButton.styleFrom(
+                    shadowColor: Colors.green,
+                    backgroundColor: Colors.deepPurple,
+                    elevation: 1.5,
+                    minimumSize: Size(150, 40)),
+              )
+            ],
+          ),
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:codepur_8h/Pages/home_page.dart';
 import 'package:codepur_8h/Pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:codepur_8h/Utiitz/routes.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-
     initialRoute: "/login",
     routes: {
       "/": (context) => LoginPage(),
-      "/home": (context) => HomePage(),
-      "/login": (context) => LoginPage(),
+      MyRoutes.homeRoute: (context) => HomePage(),
+      MyRoutes.loginRoute: (context) => LoginPage(),
     },
   ));
 }
