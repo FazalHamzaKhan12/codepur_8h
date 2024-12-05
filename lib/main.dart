33591652fb5e318b4333194989b2bebacafa1979
Fazal Hamza Khan <theunknown.pak@gmail.com>
2024-12-05 09:39:08 +0500

    Chapter 3 Completed 1:17:55 / 8:38:00

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:codepur_8h/Pages/home_page.dart';
+import 'package:codepur_8h/Pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: "/home",
+    routes: {
+      "/": (context) => LoginPage(),
+      "/home": (context) => HomePage(),
+      "/login": (context) => LoginPage(),
+    },
+  ));
 }
