16ed1d8cc41ed9785046a89da784c268b4e3da5e
Fazal Hamza Khan <theunknown.pak@gmail.com>
2024-12-17 12:10:51 +0500

    Chapter 12 Completed
    (Local Files | Load & Decode JSON)

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,18 +1,42 @@
+import 'dart:convert';
+
 import 'package:codepur_8h/Models/catalog.dart';
 import 'package:codepur_8h/Widgets/drawer.dart';
 import 'package:codepur_8h/Widgets/itemwidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData()async{
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodeJson = jsonDecode(catalogJson);
+    var productsData = decodeJson["products"];
+    print(productsData);
+
+  }
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(8, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Catgadflog"),
+        title: Text("Catgalog"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
